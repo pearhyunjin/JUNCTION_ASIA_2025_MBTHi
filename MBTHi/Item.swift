2f835c9dd98f2cbd6d2c1e4f841d702ab9a8fc77
pearhyunjin <pearhyunjin@gmail.com>
2025-08-23 21:37:40 +0900

    Initial Commit

---
+++ MBTHi/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MBTHi
+//
+//  Created by 배현진 on 8/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
